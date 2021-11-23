4b92e6592ed6f331ef00cea5892404e90de61362
Attila Oláh <atl@google.com>
2021-11-23 23:31:13 +0100

    Godot: Simplify building the mesh.
    Do not re-apply the material, instead keep the one from the editor.

--- geometry/icosahedron.gd
+++ geometry/icosahedron.gd
@@ -1,11 +1,11 @@
 class_name Icosahedron
 extends ArrayMesh
 
-const phi = PI * 2.0 / 5.0
-const theta = PI / 2.0 - atan(1.0 / 2.0)
+const PHI = PI * 2.0 / 5.0
+const THETA = PI / 2.0 - atan(1.0 / 2.0)
 
 
-func build_icosahedron(mat: Material, r: float = 1.0) -> ArrayMesh:
+func build_icosahedron(r: float = 1.0) -> ArrayMesh:
 	var st: SurfaceTool = SurfaceTool.new()
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
 
@@ -17,8 +17,8 @@ func build_icosahedron(mat: Material, r: float = 1.0) -> ArrayMesh:
 	var nr: Array = []
 	var sr: Array = []
 	for i in 5:
-		nr.append(np.rotated(Vector3.RIGHT, theta).rotated(Vector3.UP, phi * i))
-		sr.append(sp.rotated(Vector3.RIGHT, theta).rotated(Vector3.UP, phi * i))
+		nr.append(np.rotated(Vector3.RIGHT, THETA).rotated(Vector3.UP, PHI * i))
+		sr.append(sp.rotated(Vector3.RIGHT, THETA).rotated(Vector3.UP, PHI * i))
 
 	for i in 5:
 		# North 5-pyramid:
@@ -33,8 +33,8 @@ func build_icosahedron(mat: Material, r: float = 1.0) -> ArrayMesh:
 		# South prism teeth:
 		add_triangle(st, nr[(i + 1) % 5], sr[(i + 4) % 5], sr[(i + 3) % 5])
 
-	st.set_material(mat)
 	return st.commit()
+
 
 func add_triangle(st: SurfaceTool, a: Vector3, b: Vector3, c: Vector3) -> void:
 	st.add_normal(Plane(a, b, c).normal)
--- meshes/planet.gd
+++ meshes/planet.gd
@@ -2,21 +2,7 @@ class_name Planet
 extends MeshInstance
 
 onready var icos: Icosahedron = Icosahedron.new()
-onready var eqr_shader: Shader = load("res://shaders/eqr.tres")
-onready var earth_txt: StreamTexture = load("res://textures/nasa_land_ocean_ice_8192.tres")
 
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	var mat = ShaderMaterial.new()
-	mat.set_shader(eqr_shader)
-	mat.set_shader_param("texture_map", earth_txt)
-	
-	#var texture: StreamTexture = mat.get_shader_param("texture_map")
-	#print(texture)
-	set_mesh(icos.build_icosahedron(mat))
-	#mat.add_surface_from_arrays(mesh)
-	#get_mesh().surface_set_material(0, mat)
-	#print(get_mesh().get_material())
-	#mesh.surface_set_material(1, mat)
+	set_mesh(icos.build_icosahedron())
