51fb8898396b42d908f90639b0350b52a389515b
Attila Oláh <atl@google.com>
2021-11-26 23:23:25 +0100

    Godot: Precompute pentagon faces.

--- geometry/pentagon.gd
+++ geometry/pentagon.gd
@@ -9,41 +9,55 @@ const ALPHA: float = PI * 2.0 / 5.0
 const A1: float = 10.0 / sqrt(50.0 + 10.0 * sqrt(5.0))
 
 # Orientation and hight:
-var o: Vector3
+var _o: Vector3
 
 # Radius of the circumscribed circle:
-var r: float
+var _r: float
+
+# Vertices. These are computed lazily.
+# Vertex "a" is the "top", others are in clockwise order.
+var a: Vector3
+var b: Vector3
+var c: Vector3
+var d: Vector3
+var e: Vector3
+
+# Whether or not the vertices have been computed.
+var _materialized: bool = false
 
 
-func _init(o: Vector3, a: float) -> void:
-	self.o = o
-	r = a / A1
+func _init(distance: Vector3, edge_len: float) -> void:
+	_o = distance
+	_r = edge_len / A1
 
 
-# Add the pentagon to an existing mesh.
 func add_to(st: SurfaceTool) -> void:
-	var a: Vector3
-	var n: Vector3 = o.normalized()
-	if is_equal_approx(o.x, 0.0) and is_equal_approx(o.z, 0.0):
-		# North Pole "top" should point towards Vector3.BACK.
-		# South Pole "top" should point towards Vector3.FORWARD.
-		a = Vector3(o.x, o.y, sign(n.y) * r)
-	else:
-		# Icosahedron north or south ring vertex.
-		# The "top" of the pentagon should point away from the XZ plane.
-		a = o.move_toward(
-			# Intersection of the pentagon's plane with the XY and YZ planes:
-			Plane(n, o.length()).intersect_3(Plane.PLANE_XY, Plane.PLANE_YZ),
-			r  # Move by "radius" in the plane
-		)
-
-	# Rotate by a negative amount (clockwise) to match Godot's winding order.
-	var b: Vector3 = a.rotated(n, -ALPHA * 1.0)
-	var c: Vector3 = a.rotated(n, -ALPHA * 2.0)
-	var d: Vector3 = a.rotated(n, -ALPHA * 3.0)
-	var e: Vector3 = a.rotated(n, -ALPHA * 4.0)
-	print("P: R = ", a.length(), ", A = ", a.distance_to(b), ", r = ", a.distance_to(o))
-
+	if not _materialized:
+		materialize()
 	Icosahedron.add_triangle(st, a, b, c)
 	Icosahedron.add_triangle(st, a, c, d)
 	Icosahedron.add_triangle(st, a, d, e)
+
+
+func materialize() -> void:
+	var n: Vector3 = _o.normalized()
+	if is_equal_approx(_o.x, 0.0) and is_equal_approx(_o.z, 0.0):
+		# North Pole "top" should point towards Vector3.BACK.
+		# South Pole "top" should point towards Vector3.FORWARD.
+		a = Vector3(0.0, _o.y, sign(n.y) * _r)
+	else:
+		# Icosahedron north or south ring vertex.
+		# The "top" of the pentagon should point away from the XZ plane.
+		a = _o.move_toward(
+			# Intersection of the pentagon's plane with the XY and YZ planes:
+			Plane(n, _o.length()).intersect_3(Plane.PLANE_XY, Plane.PLANE_YZ),
+			_r  # Move by "radius" in the plane
+		)
+
+	# Rotate by a negative amount (clockwise) to match Godot's winding order.
+	b = a.rotated(n, -ALPHA * 1.0)
+	c = a.rotated(n, -ALPHA * 2.0)
+	d = a.rotated(n, -ALPHA * 3.0)
+	e = a.rotated(n, -ALPHA * 4.0)
+	print("P: R = ", a.length(), ", A = ", a.distance_to(b), ", r = ", a.distance_to(_o))
+	_materialized = true
